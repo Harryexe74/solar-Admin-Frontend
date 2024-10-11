faafd2226a42e5acdd415eabc2d25380c3176fbc
Harryexe74 <junaidmahmood141@gmail.com>
2024-10-11 11:01:35 +0500

    api url

--- src/ApiUrl.js
+++ src/ApiUrl.js
@@ -1,6 +1,9 @@
-// const ApiUrl = 'https://solarbackendupdated2.vercel.app/';
-const ApiUrl = 'https://solarbackend-final.vercel.app';
+// const ApiUrl = 'https://solarbackendupdated2.vercel.app';
+// const ApiUrl = 'https://solarbackend-final.vercel.app';
 // const ApiUrl = 'http://localhost:3000';
+const ApiUrl = process.env.NODE_ENV === "production"
+  ? "https://solarbackend-final.vercel.app"
+  : "http://localhost:3000";
 
 
 export default ApiUrl;
